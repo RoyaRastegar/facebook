a598812e83b679d95ffab49660026dfa564847e3
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-08-21 15:39:20 +0800

    add Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
     </div>
   );
 };
